7423260569759001b41b107bf5d2a123b36295ef
Ahmed Saied <ahmed.saeed311294@gmail.com>
2021-12-24 19:48:45 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const fs = require('fs')
+const path = require('path');
+const url = require('url')
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    let filePath = path.join(__dirname, 'pages', req.url==="/" ? "home.html" : req.url)
+    let fileExt = path.extname(filePath)
+    let contentType = 'text/html'
+    switch(fileExt){
+        case ".js":
+            contentType = 'text/javascript';
+            break;
+        case ".css":
+            contentType = 'text/css';
+            break;
+        case ".png":
+            contentType = 'image/png';
+            break;
+        case ".jpg":
+            contentType = 'image/jpg';
+            break;
+        case ".json":
+            contentType = 'application/json';
+            break;
+    }
+
+    fs.readFile(filePath, 'utf8', (err, content) => {
+        if(err){
+            if(err.code=='ENOENT'){
+                fs.readFile(path.join(__dirname, "pages", "404.html"), "utf8", (err, content) => {
+                    res.writeHead(404, {'Content-Type':'text/html'})
+                    res.end(content)
+                })
+            }else{
+                res.writeHead(500, {'Content-Type':'text/html'})
+                res.end(`<h1>Internal Server Error ${err.code}</h1>`)
+            }
+        }else{
+            fs.readFile(filePath, "utf8", (err, content) => {
+                res.writeHead(200, {'Content-Type':contentType})
+                res.end(content)
+            })
+        }
+    })
+
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`server running on port ${PORT}`))
