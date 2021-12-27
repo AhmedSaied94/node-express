f022f75ab8bddbfa915692c3db56b3b42a256c0d
Ahmed Saied <ahmed.saeed311294@gmail.com>
2021-12-27 19:06:37 +0200

    added routs, views, ejs, ejs engine

---
+++ app.js
@@ -0,0 +1,70 @@
+const fs = require('fs')
+const path = require('path');
+const url = require('url')
+const http = require('http')
+const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
+const app = express()
+
+//implement ejs as middleware
+app.use(expressLayouts)
+app.set('view engine', 'ejs');
+
+// app.use(express.static(path.join(__dirname, 'pages')))
+
+//home route
+app.use('/', require('./routers/index'))
+
+//users routes
+app.use('/users', require('./routers/users'))
+
+
+
+
+// const server = http.createServer((req, res) => {
+//     let filePath = path.join(__dirname, 'pages', req.url==="/" ? "home.html" : req.url)
+//     let fileExt = path.extname(filePath)
+//     let contentType = 'text/html'
+//     switch(fileExt){
+//         case ".js":
+//             contentType = 'text/javascript';
+//             break;
+//         case ".css":
+//             contentType = 'text/css';
+//             break;
+//         case ".png":
+//             contentType = 'image/png';
+//             break;
+//         case ".jpg":
+//             contentType = 'image/jpg';
+//             break;
+//         case ".json":
+//             contentType = 'application/json';
+//             break;
+//     }
+
+//     fs.readFile(filePath, 'utf8', (err, content) => {
+//         if(err){
+//             if(err.code=='ENOENT'){
+//                 fs.readFile(path.join(__dirname, "pages", "404.html"), "utf8", (err, content) => {
+//                     res.writeHead(404, {'Content-Type':'text/html'})
+//                     res.end(content)
+//                 })
+//             }else{
+//                 res.writeHead(500, {'Content-Type':'text/html'})
+//                 res.end(`<h1>Internal Server Error ${err.code}</h1>`)
+//             }
+//         }else{
+//             fs.readFile(filePath, "utf8", (err, content) => {
+//                 res.writeHead(200, {'Content-Type':contentType})
+//                 res.end(content)
+//             })
+//         }
+//     })
+
+// });
+
+const PORT = process.env.PORT || 5000;
+
+// server.listen(PORT, () => console.log(`server running on port ${PORT}`))
+app.listen(PORT, ()=> console.log('server is running on port '+PORT))
---
+++ routers/users.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+
+//login route
+router.get('/login', (req,res) => res.render('login'))
+
+//Register route
+router.get('/register', (req,res) => res.render('register'))
+
+
+module.exports = router
